8c2229caa8daddd175d9295f8ee6ff774b1cdd8f
Alex Leonkov <alex.leonkov@digitalich.de>
2025-10-21 19:04:54 +0200

    wp small changes

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -1,39 +1,40 @@
 let scrollPos = window.scrollY;
-const numbersSection = document.querySelector('.numbers-section');
-const numbersSectionTop = numbersSection.offsetTop + numbersSection.offsetHeight - window.innerHeight;
+const numbersSection = document.querySelector(".numbers-section");
+const numbersSectionTop =
+  numbersSection.offsetTop + numbersSection.offsetHeight - window.innerHeight;
 
 const animateNumbersSection = () => {
-    scrollPos = window.scrollY;
+  scrollPos = window.scrollY;
 
-    const numOne = document.getElementById('num-1');
-    const numTwo = document.getElementById('num-2');
-    const numThree = document.getElementById('num-3');
-    const duration = 3000;
-    const time_duration = 1000;
+  const numOne = document.getElementById("num-1");
+  const numTwo = document.getElementById("num-2");
+  const numThree = document.getElementById("num-3");
+  const duration = 3000;
+  const time_duration = 1000;
 
-    if (scrollPos >= numbersSectionTop) {
-        numbersSection.classList.add('active');
-    }
+  if (scrollPos >= numbersSectionTop) {
+    numbersSection.classList.add("active");
+  }
 
-    if (numbersSection.classList.contains('active')) {
-        animateValue(numOne, 0, 562, duration);
-        animateValue(numTwo, 0, 613, duration);
-        animateValue(numThree, 0, 3, time_duration);
-        window.removeEventListener('scroll', animateNumbersSection, true);
-    }
-}
+  if (numbersSection.classList.contains("active")) {
+    animateValue(numOne, 0, 950, duration);
+    animateValue(numTwo, 0, 1083, duration);
+    animateValue(numThree, 0, 4, time_duration);
+    window.removeEventListener("scroll", animateNumbersSection, true);
+  }
+};
 
 const animateValue = (obj, start, end, duration) => {
-    let startTimestamp = null;
-    const step = (timestamp) => {
-        if (!startTimestamp) startTimestamp = timestamp;
-        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-        obj.innerHTML = Math.floor(progress * (end - start) + start);
-        if (progress < 1) {
-            window.requestAnimationFrame(step);
-        }
-    };
-    window.requestAnimationFrame(step);
-}
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+};
 
-window.addEventListener('scroll', animateNumbersSection, true);
+window.addEventListener("scroll", animateNumbersSection, true);
